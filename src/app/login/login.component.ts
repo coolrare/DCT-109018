c0ce1cb0f05ea650ace2e602e3690a9841877316
黃升煌 Mike <mike@miniasp.com>
2020-09-06 15:40:28 +0800

    任務 04：在程式中動態切換路由

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,44 @@
+import { LoginService } from './../login.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { of, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  user = {
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor(private router: Router, private loginService: LoginService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.loginService
+      .login(this.user)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          alert(error.error.body[0]);
+          return throwError(error);
+        })
+      )
+      .subscribe({
+        next: (result) => {
+          console.log(result);
+          const token = result.user.token;
+          localStorage.setItem('token', token);
+          this.router.navigateByUrl('/');
+        },
+        error: (error: HttpErrorResponse) => {
+          console.log(error);
+        }
+      });
   }
-
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule
   ]
 })
 export class LoginModule { }
