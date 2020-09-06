0ca129c14d3e4fcff23c830071731b9f0e66dd8e
黃升煌 Mike <mike@miniasp.com>
2020-09-06 15:10:40 +0800

    修正錯字

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,15 +21,15 @@ export class AppComponent implements OnInit {
     const source$ = from([1, 2, 3, 4]).pipe(
       tap((data) => console.log(`source: ${data}`))
     );
-    source$.subscribe((data) => console.log(`沒 sharePlay, 第 1 次 subscribe: ${data}`));
-    source$.subscribe((data) => console.log(`沒 sharePlay, 第 2 次 subscribe: ${data}`));
+    source$.subscribe((data) => console.log(`沒 shareReplay, 第 1 次 subscribe: ${data}`));
+    source$.subscribe((data) => console.log(`沒 shareReplay, 第 2 次 subscribe: ${data}`));
 
     const source2$ = from([1, 2, 3, 4]).pipe(
       tap((data) => console.log(`source2: ${data}`)),
       shareReplay(3) // cache 最後三次發生的值
     );
     // 還沒 subscribe 的時候，不會執行整個 stream
-    source2$.subscribe((data) => console.log(`有 sharePlay, 第 1 次 subscribe: ${data}`));
-    source2$.subscribe((data) => console.log(`有 sharePlay, 第 2 次 subscribe: ${data}`));
+    source2$.subscribe((data) => console.log(`有 shareReplay, 第 1 次 subscribe: ${data}`));
+    source2$.subscribe((data) => console.log(`有 shareReplay, 第 2 次 subscribe: ${data}`));
   }
 }
