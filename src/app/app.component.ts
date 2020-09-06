242914b893ca7ba2e11241b190260d294d0986cf
黃升煌 Mike <mike@miniasp.com>
2020-09-06 14:55:56 +0800

    補充：shareReplay 範例

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { shareReplay } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { partition } from 'lodash';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +12,18 @@ export class AppComponent implements OnInit {
   title = 'angular-realworld-advanced-starter';
 
   ngOnInit(): void {
-    console.log($('body'));
-    console.log(partition([1, 2, 3, 4], n => n % 2));
+    // console.log($('body'));
+    // console.log(partition([1, 2, 3, 4], n => n % 2));
+
+    const source$ = from([1, 2, 3, 4]).pipe(
+      shareReplay(3) // cache 最後三次發生的值
+    );
+
+    source$.subscribe(data => console.log(data));
+    // from([1, 2, 3, 4]).subscribe(data => console.log(data));
+
+
+
   }
 }
 
