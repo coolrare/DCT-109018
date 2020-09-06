ce1e26ea3ca68825d9a6f5c3a6b5130a2277a6b7
黃升煌 Mike <mike@miniasp.com>
2020-09-06 15:08:24 +0800

    補充: shareReplay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { shareReplay } from 'rxjs/operators';
+import { PostService } from './post.service';
+import { delay, shareReplay, tap } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { partition } from 'lodash';
 import { from } from 'rxjs';
@@ -6,25 +7,29 @@ import { from } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'angular-realworld-advanced-starter';
+
+  constructor() {}
 
   ngOnInit(): void {
     // console.log($('body'));
     // console.log(partition([1, 2, 3, 4], n => n % 2));
 
     const source$ = from([1, 2, 3, 4]).pipe(
+      tap((data) => console.log(`source: ${data}`))
+    );
+    source$.subscribe((data) => console.log(`沒 sharePlay, 第 1 次 subscribe: ${data}`));
+    source$.subscribe((data) => console.log(`沒 sharePlay, 第 2 次 subscribe: ${data}`));
+
+    const source2$ = from([1, 2, 3, 4]).pipe(
+      tap((data) => console.log(`source2: ${data}`)),
       shareReplay(3) // cache 最後三次發生的值
     );
-
-    source$.subscribe(data => console.log(data));
-    // from([1, 2, 3, 4]).subscribe(data => console.log(data));
-
-
-
+    // 還沒 subscribe 的時候，不會執行整個 stream
+    source2$.subscribe((data) => console.log(`有 sharePlay, 第 1 次 subscribe: ${data}`));
+    source2$.subscribe((data) => console.log(`有 sharePlay, 第 2 次 subscribe: ${data}`));
   }
 }
-
-
