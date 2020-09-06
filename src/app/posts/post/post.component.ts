6c557a49d27f9ed53b3d7a70bc8fd2ad85778388
黃升煌 Mike <mike@miniasp.com>
2020-09-06 14:55:15 +0800

    任務 03：在程式中取得路由參數

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,14 +1,46 @@
+import { Article } from './../../interfaces/article';
+import { PostService } from './../../post.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, map, shareReplay } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
+  // article: Article;
+  article$: Observable<Article>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
 
   ngOnInit(): void {
-  }
+    // this.route.paramMap.subscribe((paramMap) => {
+    //   console.log(paramMap.get('id'));
+    //   this.postService.getArticle(paramMap.get('id')).subscribe((result) => {
+    //     this.article = result.article;
+    //     console.log(this.article);
+    //   });
+    // });
 
+    // this.route.paramMap
+    //   .pipe(
+    //     map((paramMap) => paramMap.get('id')),
+    //     switchMap((id) => this.postService.getArticle(id))
+    //   )
+    //   .subscribe((result) => {
+    //     console.log(result);
+    //   });
+
+    this.article$ = this.route.paramMap.pipe(
+      map((paramMap) => paramMap.get('id')),
+      switchMap((id) => this.postService.getArticle(id)),
+      map((result) => result.article),
+     // shareReplay(1)
+    );
+  }
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,33 @@
+import { Article } from './../interfaces/article';
+import { PostService } from './../post.service';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
+  articles: Article[];
 
-  constructor() { }
+  articles$: Observable<Article[]>;
+
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
-  }
+    this.articles$ = this.postService
+      .getArticles()
+      .pipe(map((result) => result.articles));
 
+    this.postService
+      .getArticles()
+      .pipe(map((result) => result.articles))
+      .subscribe((articles) => {
+        console.log(articles);
+        this.articles = articles;
+        // this.articles = result.articles;
+      });
+  }
 }
